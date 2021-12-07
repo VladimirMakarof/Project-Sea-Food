7c9488f902990bc249d8ccc2a824d52b3776b9e3
VladimirMakarof <stan13.09@mail.ru>
2021-12-07 10:49:02 +0300

    feat: add adaptive version

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ dots: true,
 prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
 nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
 vertical: true,
+responsive: [
+  {
+  breakpoint: 371,
+  settings:{
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  }
+}
+]
 
 });
 
@@ -17,6 +28,24 @@ $('.product__name').slick({
     vertical: true,
     prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
     nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    responsive: [
+      {
+      breakpoint: 891,
+      settings:{
+        vertical: false,
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+      },
+      breakpoint: 461,
+      settings:{
+        vertical: false,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+      }
+    }
+    ]
   });
   $('.product__content').slick({
     slidesToShow: 1,
@@ -24,6 +53,10 @@ $('.product__name').slick({
     asNavFor: '.product__name',
     fade: true,
     arrows: false,
+  });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
   });
 
 });
